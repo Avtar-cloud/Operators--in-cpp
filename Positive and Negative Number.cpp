b12b55a801e2220799b3be93496d0ce333fccd6c
Avtar-cloud <avtar.singh.btech2024@sitpune.edu.in>
2025-07-21 21:43:56 +0530

    Create Positive and Negative Number.cpp

---
+++ Positive and Negative Number.cpp
@@ -0,0 +1,17 @@
+//AVTAR SINGH ENTC A1
+//24070123027
+#include <iostream>
+using namespace std;
+int main(){
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+    if (number > 0) {
+        cout << "The number is positive." << endl;
+    } else if (number < 0) {
+        cout << "The number is negative." << endl;
+    } else {
+        cout << "The number is zero itself." << endl;
+    }
+    return 0;
+}
